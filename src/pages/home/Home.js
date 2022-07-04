2223d0def3bf7c4797cd1a9e97ebaa03858d3424
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-04 12:14:22 -0500

    Clase 82

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,16 @@
+import { useFetch } from './../../hooks/useFetch'
+
 import './Home.css'
 
 export const Home = (props) => {
+  const { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes')
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipes && recipes.map(recipe => (
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
+    </div>
   )
 }
