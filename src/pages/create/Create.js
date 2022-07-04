a9701a679a03ae07c7f73506b7f59f1d25b306d2
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-04 13:11:44 -0500

    Clase 86

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,58 @@
+import { useState } from 'react'
+
 import './Create.css'
 
 export const Create = (props) => {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title, method, cookingTime)
+  }
+
   return (
-    <div>Create</div>
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={ (event) => setTitle(event.target.value) }
+            value={ title }
+            required
+          />
+        </label>
+
+        {/* ingredients go here */}
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            cols="30"
+            rows="10"
+            onChange={ (event) => setMethod(event.target.value) }
+            value={ method }
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time:</span>
+          <input
+            type="number"
+            onChange={ (event) => setCookingTime(event.target.value)}
+            value={ cookingTime }
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+
+      </form>
+    </div>
   )
 }
