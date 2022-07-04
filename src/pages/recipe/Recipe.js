14c9c5df40eca39965caf7ac124877ae22ce7e0f
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-04 12:48:11 -0500

    Clase 84 - challenge

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,20 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from './../../hooks/useFetch'
+
 import './Recipe.css'
 
 export const Recipe = (props) => {
+  const { id } = useParams()
+  const url = `http://localhost:3000/recipes/${id}`
+  const { data: recipe, isPending, error } = useFetch(url)
+
   return (
-    <div>Recipe</div>
+    <div>
+      { isPending && <div>Loading...</div> }
+      { error && <div>{error}</div> }
+      { recipe && (
+        <h1>{recipe.title}</h1>
+      )}
+    </div>
   )
 }
