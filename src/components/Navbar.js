6920ac26d986e894d33e0c60baf8ba416b626406
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-05 10:13:46 -0500

    clase 96

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
 
 import { SearchBar } from './SearchBar'
+import { ThemeContext } from '../context/ThemeContext'
 
 import './Navbar.css'
 
 export const Navbar = () => {
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ backgroundColor: color}}>
       <nav>
         <Link to='/' className='brand'>
           <h1>Cooking Developer</h1>
