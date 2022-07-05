daeabdbbe07608d008ccd26e4e941bc23f7ea35d
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-05 16:38:40 -0500

    Clase 106

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,14 +1,31 @@
+import { projectFirestore } from '../../firebase/config'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { useFetch } from './../../hooks/useFetch'
 import { useTheme } from './../../hooks/useTheme'
 
 import './Recipe.css'
 
 export const Recipe = () => {
   const { id } = useParams()
-  const url = `http://localhost:3000/recipes/${id}`
-  const { data: recipe, isPending, error } = useFetch(url)
   const { mode } = useTheme()
+
+  const [recipe, setRecipe] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').doc(id).get().then(doc => {
+      if(doc.exists) {
+        setIsPending(false)
+        setRecipe(doc.data())
+      } else {
+        setIsPending(false)
+        setError('Could not find that recipe')
+      }
+    })
+  }, [id])
 
   return (
     <div className={`recipe ${mode}`}>
